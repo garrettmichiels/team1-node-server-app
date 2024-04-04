04e901c0141ed4a64726aaa9a253055af5643f70
Maggie Collette <collette.m@northeastern.edu>
2024-04-04 17:24:56 -0400

    Update schema.js

--- Companies/schema.js
+++ Companies/schema.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const companySchema = mongoose.Schema(
   {
     id: String,
-    company_name: String, 
-    company_id: String, 
+    companyName: String, 
+    companyId: String, 
     reviews: [{ref: "Reviews", type: mongoose.Schema.Types.ObjectId}]
   }
 );
