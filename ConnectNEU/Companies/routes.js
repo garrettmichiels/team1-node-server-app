3e8e9fbc7e1563bded13a15202ee748e2828410f
Maggie Collette <collette.m@northeastern.edu>
2024-04-19 13:06:15 -0400

    company routes/dao

---
+++ ConnectNEU/Companies/routes.js
@@ -0,0 +1,34 @@
+import * as dao from "./dao.js";
+
+export default function CompanyRoutes(app) {
+    const createCompany = async (req, res) => { 
+        const company = await dao.createCompany(req.body)
+        res.json(company)
+    }
+    const findCompanyById = async (req, res) => { 
+        const {companyId} = req.params
+        const company = await dao.findCompanyById(companyId)
+        res.json(company)
+    }
+    const findAllCompanies = async (req, res) => { 
+        const companies = await dao.findAllCompanies()
+        res.json(companies)
+    }
+    const addCompanyReview = async (req, res) => { 
+        const {companyId} = req.params
+        const status = await dao.addReview(companyId, req.body)
+        const company = await dao.findCompanyById(companyId)
+        res.json(company)
+    }
+    const deleteCompanyReview = async (req, res) => { 
+        const {companyId, reviewId} = req.params
+        const status = await dao.deleteCompanyReview(companyId, reviewId)
+        res.json(status)
+    }
+
+    app.put("/api/companies/:companyId", addCompanyReview)
+    app.delete("/api/companies/:companyId/:reviewId", deleteCompanyReview)
+    app.post("/api/companies/", createCompany)
+    app.get("/api/companies/:companyId", findCompanyById)
+    app.get("/api/companies", findAllCompanies)
+}
