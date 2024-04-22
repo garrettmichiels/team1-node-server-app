6811abc4ee202c56a4331aaf3f0c06bcb61fd90c
garrettmichiels <michielsgarrett@gmail.com>
2024-04-22 14:40:26 -0400

    fix conflicts

--- ConnectNEU/Users/schema.js
+++ ConnectNEU/Users/schema.js
@@ -12,6 +12,7 @@ const userSchema = new mongoose.Schema({
     // id: {type: String, unique: true, required:true},
   //  industry: String, //(Only for Mentors)
     major : String,
+    job: [],
      companies: [{ref: 'companies',type: mongoose.Schema.Types.ObjectId}],
       following: [{ref: 'users', type: mongoose.Schema.Types.ObjectId}],
       reviews: [{ref: 'reviews', type: mongoose.Schema.Types.ObjectId}]
